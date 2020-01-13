6a63175257137578e2ea7ff348f17ce98f4e1da1
Debabrata Sahoo <debabrata@prmfincon.com>
2020-01-13 15:00:01 +0530

    change in code

--- src/test/java/com/algorithm/main/AlgorithmApplicationTests.java
+++ src/test/java/com/algorithm/main/AlgorithmApplicationTests.java
@@ -1,6 +1,7 @@
 package com.algorithm.main;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
